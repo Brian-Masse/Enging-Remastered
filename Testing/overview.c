3cccc24f3c29f206962c060ff8ea6d71f5f64bd1
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2022-11-30 11:10:13 -0500

    added basic c examples, added initial planning documents

---
+++ Testing/overview.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// BASIC TYPES
+
+// char
+
+// int
+// float
+// double
+// short / long
+
+// strings are arrays of chars, ending in a null character
+// each char has its own memory address
+
+// * marks a variable as a pointer
+// malloc - allocates memory
+// free - frees memory
+
+int testFunction()
+{
+    printf("the function run!");
+    return 0;
+}
+
+int main()
+{
+
+        printf("Hello World!\n");
+    testFunction();
+
+    return 0;
+}
---
+++ Testing/window/window.c
@@ -0,0 +1,12 @@
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    glutInit(&argc, argv);
+
+    return 0;
+}
