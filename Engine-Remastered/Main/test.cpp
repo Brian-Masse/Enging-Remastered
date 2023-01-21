20745c64dd2c09a09ff4b44cfed65ab218c2cb64
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2023-01-21 15:16:42 -0500

    configured launch.json, tasks.json, added pre/build tasks, put in boiler plate vulkan code

--- Engine-Remastered/Main/test.cpp
+++ Engine-Remastered/Main/test.cpp
@@ -1,30 +1,91 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>  // for error handling
 #include <stdexcept> // for error handling
-#include <cstdlib>
+#include <cstdlib>   // EXIT SUCCESS / FAILURE macros
+
+using namespace std;
 
 class HelloTriangleApplication
 {
 public:
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    // constants:
+
+    const int WIDTH = 600;
+    const int HEIGHT = 600;
+
+    GLFWwindow *window;
+    VkInstance instance;
+
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // prevents it from initializing OpenGL
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Window", nullptr, nullptr);
+    }
+
     void initVulkan()
     {
+        createInstance();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Engine-Remastered";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // tells the compiler the global extension / validation layers to use
+        // global - applies to the entire program, not a specific computer
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0; // number of validation layers
+
+        // pointer to information, custom allocator callbacks, and an instance to store the VKInstance (global class property)
+        // VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        // if (result != VK_SUCCESS)
+        // {
+        //     throw std::runtime_error("failed to create instance!");
+        // }
     }
 
     void mainLoop()
     {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 };
 
