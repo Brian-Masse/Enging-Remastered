bdc96230e2f60f13813b4f8d082359e97b659ffd
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2023-01-23 00:46:34 -0500

    added surface initialization, and swapchain creation, access, and configuration

--- Engine-Remastered/Main/test.cpp
+++ Engine-Remastered/Main/test.cpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <cstring>
 #include <optional>
+#include <set>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -54,13 +57,20 @@ private:
     const bool enableValidationLayers = true;
 #endif
 
-    GLFWwindow *window;
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessenger; 
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     VkDevice device;
 
+    GLFWwindow *window;
+    VkSurfaceKHR surface;
+    VkSwapchainKHR swapChain;
+    vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+
     VkQueue graphicsQueue;
+    VkQueue presentQueue;
 
     // MARK: Initialization
     void initWindow()
@@ -75,9 +85,11 @@ private:
     void initVulkan()
     {
         createInstance();
+        createSurface();
         setupMessenger();
         pickPhysicalDevice();
         createLogicalDevice();
+        createSwapChain();
     }
 
     void createInstance()
@@ -123,6 +135,12 @@ private:
         }
     }
 
+    //MARK: Window
+    void createSurface() {
+        VkResult result = glfwCreateWindowSurface(instance, window, nullptr, &surface);
+        if (result != VK_SUCCESS) { throw runtime_error("Failed to create the surface"); }
+    }
+
     // MARK: Validation Layers
     bool checkValidationLayerSupport()
     {
@@ -151,7 +169,7 @@ private:
         //this lists the types of messages (severity and type) that this messenger can receive
         createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
         createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
         createInfo.pfnUserCallback = debugCallback;
     }
@@ -197,7 +215,7 @@ private:
         return VK_FALSE;
     }
 
-    //MARK: Physical Device Selection:
+    //MARK: Physical Device:
     void pickPhysicalDevice() {
         uint32_t deviceCount = 0;
         vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
@@ -226,17 +244,167 @@ private:
         vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
         //preform an special checks for the device using the above information
-
         QueueFamilyIndicies indicies = findQueueFamilies(device);
 
-        return indicies.graphicsFamily.has_value();
+        bool extensionsSupported = checkDeviceExtensionSupport(device);
 
+        bool swapChainAdequate = false;
+        if (extensionsSupported) {
+            SwapChainSupportDetails details = querySwapChainSupport(device);
+            if ( !details.formats.empty() && !details.presentMode.empty() ) { swapChainAdequate = true; }
+        }
+
+        return indicies.isComplete() && extensionsSupported && swapChainAdequate;
     }
+
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device) {
+        uint32_t extensionCount;
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+        vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateDeviceExtensionProperties( device, nullptr, &extensionCount, availableExtensions.data() );
+
+        set<string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+
+        for ( const auto& extension : availableExtensions ) { requiredExtensions.erase( extension.extensionName ); }
+        return requiredExtensions.empty();
+    }
+
+    //swapchain details
+    const vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    struct SwapChainSupportDetails {
+        VkSurfaceCapabilitiesKHR capabilities;        //num of images, size of images, 
+        vector<VkSurfaceFormatKHR> formats;           //color space, pixel format
+        vector<VkPresentModeKHR> presentMode;
+    };
+
+    SwapChainSupportDetails querySwapChainSupport( VkPhysicalDevice device ) {
+        SwapChainSupportDetails details;
+
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+        
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+        if (formatCount != 0) {
+            details.formats.resize( formatCount );
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data() );
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+        if (formatCount != 0) {
+            details.presentMode.resize( presentModeCount );
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentMode.data());
+        }
+        return details;
+    }
+
+    //selecting an optimal surface Format for the swapchain
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat( const vector<VkSurfaceFormatKHR>& availableFormats ) {
+        for (const auto& availableFormat: availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) { // prefer to work with 32 bit SRGB
+                return availableFormat;
+            }
+        }
+        return availableFormats[0]; // could be ranked, but this is just the first suitable one
+    }
+
+    VkPresentModeKHR chooseSurfacePresentMode( const vector<VkPresentModeKHR>& availablePresentModes ) {
+        for ( const auto& availablePresentMode : availablePresentModes ) {
+            if ( availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) { return availablePresentMode; } // this is less energy efficient, but avoid latency
+        }
+        return VK_PRESENT_MODE_FIFO_KHR;
+    }
+
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+        if (capabilities.currentExtent.width != numeric_limits<uint32_t>::max()) { return capabilities.currentExtent; }
+        else {
+            int width, height; 
+
+            glfwGetFramebufferSize(window, &width, &height);
+
+            VkExtent2D actualExtent = {
+                static_cast<uint32_t>(width),
+                static_cast<uint32_t>(height)
+            };
+
+            actualExtent.width = clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+            actualExtent.height = clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+            return actualExtent;
+        }
+    }
+
+    void createSwapChain() {
+        SwapChainSupportDetails details = querySwapChainSupport(physicalDevice);
+
+        VkSurfaceFormatKHR format = chooseSwapSurfaceFormat(details.formats);
+        VkPresentModeKHR presentMode = chooseSurfacePresentMode(details.presentMode);
+        VkExtent2D swapExtent = chooseSwapExtent(details.capabilities);
+
+        uint32_t imageCount = details.capabilities.minImageCount + 1;
+        if (details.capabilities.maxImageCount > 0 && details.capabilities.maxImageCount) { imageCount = details.capabilities.maxImageCount; }
+
+        VkSwapchainCreateInfoKHR createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        createInfo.surface = surface;
+
+        createInfo.minImageCount = imageCount;
+        createInfo.imageFormat = format.format;
+        createInfo.imageColorSpace = format.colorSpace;
+        createInfo.imageExtent = swapExtent;
+        createInfo.imageArrayLayers = 1;
+        createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+        QueueFamilyIndicies indices = findQueueFamilies(physicalDevice);
+        uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        //specifies how images are shared between queue families
+        if (indices.graphicsFamily != indices.presentFamily) {
+            createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+            createInfo.queueFamilyIndexCount = 2;
+            createInfo.pQueueFamilyIndices = queueFamilyIndices;
+        } else {
+            createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+            createInfo.queueFamilyIndexCount = 0; // Optional
+            createInfo.pQueueFamilyIndices = nullptr; // Optional
+        }
+
+        createInfo.preTransform = details.capabilities.currentTransform;
+        createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // transparency in window systems
+        createInfo.presentMode = presentMode;
+        createInfo.clipped = VK_TRUE;
+
+        createInfo.oldSwapchain = VK_NULL_HANDLE; //when new swapchains are created (window resize) it needs to reference the old one
+
+        VkResult results = vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain);
+        if (results != VK_SUCCESS) { throw runtime_error( "Error creating the swapchain (cry)" ); }
+
+        uint32_t swapChainImagesCount;
+        vkGetSwapchainImagesKHR(device, swapChain, &swapChainImagesCount, nullptr);
+        swapChainImages.resize(swapChainImagesCount);
+        vkGetSwapchainImagesKHR(device, swapChain, &swapChainImagesCount, swapChainImages.data());
+
+        swapChainImageFormat = format.format;
+        swapChainExtent = swapExtent;
+    } 
+
 
     //all actions are handled and put into queues, queues can only accept certain commands as a function of what queue family they are a part of
     // have to find what queue families are supported on each device
     struct QueueFamilyIndicies {
+        //these could be the same families, adding logic to ensure this might improve performance
         optional<uint32_t> graphicsFamily;
+        optional<uint32_t> presentFamily;
+
+        bool isComplete() {
+            return graphicsFamily.has_value() && presentFamily.has_value();
+        }
     };
 
     QueueFamilyIndicies findQueueFamilies( VkPhysicalDevice device ) {
@@ -250,7 +418,14 @@ private:
 
         int i = 0;
         for (const auto& queueFamily : queueFamilies) {
-            if ( queueFamily.queueFlags && VK_QUEUE_GRAPHICS_BIT ) { indicies.graphicsFamily = i; break; }
+            if ( queueFamily.queueFlags && VK_QUEUE_GRAPHICS_BIT ) { indicies.graphicsFamily = i; }
+
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+            if (presentSupport) { indicies.presentFamily = i; }
+
+            if (indicies.isComplete()) { break;}
+
             i ++;
         }
 
@@ -262,27 +437,40 @@ private:
         QueueFamilyIndicies indices = findQueueFamilies(physicalDevice);
 
         //the queues we want to create in a specific family
+        vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        float queuePriority = 1.0f;
+        for (uint32_t queueFamily : uniqueQueueFamilies) {
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = queueFamily;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+
         VkDeviceQueueCreateInfo queueCreateInfo{};
         queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
         queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
         queueCreateInfo.queueCount = 1;
 
-        float queuePriority = 1.0f;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
-
-        VkPhysicalDeviceFeatures deviceFeatures; // specify the features you want
+        VkPhysicalDeviceFeatures deviceFeatures{}; // specify the features you want
 
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-        
-        createInfo.queueCreateInfoCount = 1;
-        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = queueCreateInfos.size();
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
         createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = deviceExtensions.size(); //enables all the deviceExtensions (including swapchain)
+        createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
         VkResult result = vkCreateDevice(physicalDevice, &createInfo, nullptr, &device);
         if (result != VK_SUCCESS) { throw runtime_error("Failed to create Logical Device"); }
 
         vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+        vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
 
     }
 
@@ -299,7 +487,9 @@ private:
     {
         if (enableValidationLayers) { DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr); }
 
+        vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
+        vkDestroySurfaceKHR(instance, surface, nullptr);
         vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
